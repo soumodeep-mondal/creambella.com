cfb4c007016e8b1e003ac3e5b73532888a677a55
Soumodeep Mondal <106864038+soumodeep-mondal@users.noreply.github.com>
2023-02-16 12:34:40 +0530

    Add files via upload

---
+++ java/loader&2.js
@@ -0,0 +1,11 @@
+		// $(document).ready(function(){
+			// 	$('div#loading').removeAttr('id');
+		// });
+		var preloader = document.getElementById("loader_page");
+		// window.addEventListener('load', function(){
+		// 	preloader.style.display = 'none';
+		// 	})
+
+		function myFunction(){
+			preloader.style.display = 'none';
+		};
